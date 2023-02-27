24d9b3ca6cb0a63018b32f930154e5b5ab396a36
konczosi <47603441+konczosi@users.noreply.github.com>
2023-02-27 22:04:36 +0100

    Add review_penalizer() function

--- imdbquest/adjust_ratings.py
+++ imdbquest/adjust_ratings.py
@@ -1,5 +1,4 @@
-
-def oscar_calculator(oscars_count:int) -> float:
+def oscar_calculator(oscars_count: int) -> float:
     """Adjust movies ratings based on number of Oscars won"""
     rating_adjustment = 0
     if (oscars_count >= 1) and (oscars_count <= 2):
@@ -10,4 +9,11 @@ def oscar_calculator(oscars_count:int) -> float:
         rating_adjustment = 1.0
     elif oscars_count >= 10:
         rating_adjustment = 1.5
+    return rating_adjustment
+
+def review_penalizer(review_max: int, review_count: int) -> float:
+    """Adjust movie ratings based on number of reviews"""
+    rating_adjustment = 0
+    review_diff = review_max - review_count
+    rating_adjustment = (review_diff // 100_000) * 0.1
     return rating_adjustment
