af28d46b1131c985b0f285f9a4ed13b8c097e53e
konczosi <47603441+konczosi@users.noreply.github.com>
2023-02-27 18:44:10 +0100

    Add adjust_ratings module and oscal_calculator() function

---
+++ imdbquest/adjust_ratings.py
@@ -0,0 +1,13 @@
+
+def oscar_calculator(oscars_count:int) -> float:
+    """Adjust movies ratings based on number of Oscars won"""
+    rating_adjustment = 0
+    if (oscars_count >= 1) and (oscars_count <= 2):
+        rating_adjustment = 0.3
+    elif (oscars_count >= 3) and (oscars_count <= 5):
+        rating_adjustment = 0.5
+    elif (oscars_count >= 6) and (oscars_count <= 10):
+        rating_adjustment = 1.0
+    elif oscars_count >= 10:
+        rating_adjustment = 1.5
+    return rating_adjustment
